368937f83df5af00d36fb3975cf89325ea4f004e
Mantong Zhao <zhaomantong0203@gmail.com>
2019-07-17 23:40:01 -0700

    react

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import Results from './components/Results';
+import Footer from './components/Footer';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    isLoading: false,
+    error: null,
+    hasError: false,
+    searchType: 'people',
+    searchCompleted: false,
+    results: {}
+  }
+
+  handleFetch = (searchTerm, searchTypeTerm) => {
+
+    this.setState({
+      isLoading: true,
+      error: null,
+    })
+
+    const searchType = encodeURI(searchTypeTerm)
+    const searchInput = encodeURI(searchTerm)
+    const url = `https://swapi.co/api/${searchType}/?search=${searchInput}`
+    console.log(url);
+    fetch(url)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error(res.statusText)
+      })
+      .then(resJson => {
+        this.setState({
+          isLoading: false,
+          results: resJson
+        })
+      })
+      .catch(error => {
+        this.setState({
+          error: error.message,
+          hasError: true,
+          isLoading: false,
+        })
+      })
+  }
+
+  updateSearchType = (userInput) => {
+    this.setState({
+      searchType: userInput,
+    })
+  }
+
+  handleSubmit = (event, searchType, searchInput) => {
+    event.preventDefault();
+
+    document.getElementById('search-input').value = '';
+    this.updateSearchType(searchType)
+   
+    this.handleFetch(searchInput, searchType);
+
+    this.setState({
+      searchCompleted: true,
+    }) 
+  }
+
+  handleLoadMore = (url) => {
+    this.setState({
+      isLoading: true,
+      error: null,
+    })
+    fetch(url)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error(res.statusText)
+      })
+      .then(resJson => {
+        this.setState({
+          isLoading: false,
+          results: resJson
+        })
+      })
+      .catch(error => {
+        this.setState({
+          error: error.message,
+          hasError: true,
+          isLoading: false,
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className='app-container'>
+        <div className="App">
+          <Header />
+          <SearchBar 
+            handleSubmit={this.handleSubmit}
+            />
+          <Results 
+            searchType={this.state.searchType} 
+            results={this.state.results} 
+            hasError={this.state.hasError} 
+            isLoading={this.state.isLoading}
+            searchCompleted={this.state.searchCompleted}
+            handleLoadMore={this.handleLoadMore}
+            />
+          <Footer />
+        </div>
+      </div>
+    );
+  } 
+}
+
+export default App;
